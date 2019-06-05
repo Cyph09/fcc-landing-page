10cfd577dde4bd54f6a7a74445123a18e2c3a94d
Swaleh Mhina <cyph09@gmail.com>
2019-06-06 02:47:04 +0300

    download & pricing section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 let mainNav = document.querySelector(".main-nav");
 let toggleBtn = document.querySelector(".toggle-btn");
+let icon = document.querySelector(".toggle-btn i");
 
 console.log(mainNav);
 console.log(toggleBtn);
+console.log(icon);
 
 toggleBtn.addEventListener("click", () => {
   if (!mainNav.classList.contains("open")) {
     mainNav.classList.add("open");
+    icon.classList.add("fa-times");
+    icon.classList.remove("fa-bars");
   } else {
     mainNav.classList.remove("open");
+    icon.classList.add("fa-bars");
+    icon.classList.remove("fa-times");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  // const scrolled = window.scrollY;
+  // console.log(scrolled);
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".main-header").classList.add("main-header__shrink");
+  } else {
+    document
+      .querySelector(".main-header")
+      .classList.remove("main-header__shrink");
   }
 });
